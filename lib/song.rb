97945205b5b9beae02c5fbd4f7d08120a0d15103
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-04 05:52:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,25 @@ class Song
   end 
   
   def self.find_by_name(song_name)
+    self.all.detect{|song| song.name == song_name}
   end 
+  
+  def self.find_or_create_by_name(song_name)
+  self.find_by_name(song_name) || self.create_by_name(song_name)
+  end 
+  
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end 
+  
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = (filename.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ")[0])
+    song 
+  end 
+  
+  
   
 end
 
